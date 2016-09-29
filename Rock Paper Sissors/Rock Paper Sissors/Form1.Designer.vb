5bbd9152dd7c0ae424d0adb815a7f3a94f224625
Jack-Daily <jd306127@students.davenport.k12.ia.us>
2016-09-29 11:14:42 -0500

    Add files via upload

---
+++ Rock Paper Sissors/Rock Paper Sissors/Form1.Designer.vb
@@ -0,0 +1,191 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.rockRadioButton = New System.Windows.Forms.RadioButton()
+        Me.paperRadioButton = New System.Windows.Forms.RadioButton()
+        Me.scissorsRadioButton = New System.Windows.Forms.RadioButton()
+        Me.resultLabel = New System.Windows.Forms.Label()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        Me.PictureBox3 = New System.Windows.Forms.PictureBox()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(55, 416)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(93, 35)
+        Me.btnExit.TabIndex = 3
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(236, 416)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(93, 35)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "Go!"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.SystemColors.Control
+        Me.Label1.Cursor = System.Windows.Forms.Cursors.Default
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(259, 17)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(203, 25)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "Rock, Paper, Scissors"
+        '
+        'rockRadioButton
+        '
+        Me.rockRadioButton.AutoSize = True
+        Me.rockRadioButton.Location = New System.Drawing.Point(97, 112)
+        Me.rockRadioButton.Name = "rockRadioButton"
+        Me.rockRadioButton.Size = New System.Drawing.Size(51, 17)
+        Me.rockRadioButton.TabIndex = 6
+        Me.rockRadioButton.TabStop = True
+        Me.rockRadioButton.Text = "Rock"
+        Me.rockRadioButton.UseVisualStyleBackColor = True
+        '
+        'paperRadioButton
+        '
+        Me.paperRadioButton.AutoSize = True
+        Me.paperRadioButton.Location = New System.Drawing.Point(286, 112)
+        Me.paperRadioButton.Name = "paperRadioButton"
+        Me.paperRadioButton.Size = New System.Drawing.Size(53, 17)
+        Me.paperRadioButton.TabIndex = 7
+        Me.paperRadioButton.TabStop = True
+        Me.paperRadioButton.Text = "Paper"
+        Me.paperRadioButton.UseVisualStyleBackColor = True
+        '
+        'scissorsRadioButton
+        '
+        Me.scissorsRadioButton.AutoSize = True
+        Me.scissorsRadioButton.Location = New System.Drawing.Point(490, 112)
+        Me.scissorsRadioButton.Name = "scissorsRadioButton"
+        Me.scissorsRadioButton.Size = New System.Drawing.Size(64, 17)
+        Me.scissorsRadioButton.TabIndex = 8
+        Me.scissorsRadioButton.TabStop = True
+        Me.scissorsRadioButton.Text = "Scissors"
+        Me.scissorsRadioButton.UseVisualStyleBackColor = True
+        '
+        'resultLabel
+        '
+        Me.resultLabel.BackColor = System.Drawing.SystemColors.ActiveBorder
+        Me.resultLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.resultLabel.Location = New System.Drawing.Point(221, 269)
+        Me.resultLabel.Name = "resultLabel"
+        Me.resultLabel.Size = New System.Drawing.Size(293, 67)
+        Me.resultLabel.TabIndex = 9
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(55, 135)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(146, 108)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 10
+        Me.PictureBox1.TabStop = False
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(236, 135)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(146, 108)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 11
+        Me.PictureBox2.TabStop = False
+        '
+        'PictureBox3
+        '
+        Me.PictureBox3.Image = CType(resources.GetObject("PictureBox3.Image"), System.Drawing.Image)
+        Me.PictureBox3.Location = New System.Drawing.Point(443, 135)
+        Me.PictureBox3.Name = "PictureBox3"
+        Me.PictureBox3.Size = New System.Drawing.Size(146, 108)
+        Me.PictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox3.TabIndex = 12
+        Me.PictureBox3.TabStop = False
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(682, 24)
+        Me.MenuStrip1.TabIndex = 13
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(682, 494)
+        Me.Controls.Add(Me.PictureBox3)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.resultLabel)
+        Me.Controls.Add(Me.scissorsRadioButton)
+        Me.Controls.Add(Me.paperRadioButton)
+        Me.Controls.Add(Me.rockRadioButton)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox3, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents rockRadioButton As System.Windows.Forms.RadioButton
+    Friend WithEvents paperRadioButton As System.Windows.Forms.RadioButton
+    Friend WithEvents scissorsRadioButton As System.Windows.Forms.RadioButton
+    Friend WithEvents resultLabel As System.Windows.Forms.Label
+    Friend WithEvents PictureBox1 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox2 As System.Windows.Forms.PictureBox
+    Friend WithEvents PictureBox3 As System.Windows.Forms.PictureBox
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+
+End Class
---
+++ Rock Paper Sissors/Rock Paper Sissors/Form1.vb
@@ -0,0 +1,54 @@
+﻿Public Class Form1
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+
+        Randomize()
+
+        Dim num As Integer ' varvariable to hold random number (computer choice)
+        Const ROCK = 1
+        Const PAPER = 2
+        Const SCISSORS = 3
+
+        num = CInt((3 * Rnd())) ' generate random numbers 1-3
+        Dim winCount As Integer = 0
+        Dim matchCount As Integer = 0
+        Dim compCount As Integer = 0
+
+        If rockRadioButton.Checked And num = ROCK Then
+            resultLabel.Text = "Computer chose Rock too. Tie!"
+        ElseIf rockRadioButton.Checked And num = PAPER Then
+            resultLabel.Text = "Computer chose paper. You Lose!"
+            compCount += 1
+        ElseIf rockRadioButton.Checked And num = SCISSORS Then
+            resultLabel.Text = "Computer threw scissors. You Win!"
+            winCount += 1
+        ElseIf paperRadioButton.Checked And num = ROCK Then
+            resultLabel.Text = "Computer chose Rock. You Win!"
+            winCount += 1
+        ElseIf paperRadioButton.Checked And num = PAPER Then
+            resultLabel.Text = "Computer chose paper too. Tie!"
+        ElseIf paperRadioButton.Checked And num = SCISSORS Then
+            resultLabel.Text = "Computer threw scissors. You Lose!"
+            compCount += 1
+
+        ElseIf scissorsRadioButton.Checked And num = ROCK Then
+            resultLabel.Text = "Computer chose Rock. You Lose!"
+            compCount += 1
+        ElseIf scissorsRadioButton.Checked And num = PAPER Then
+            resultLabel.Text = "Computer chose paper. You Win!"
+            winCount += 1
+        ElseIf scissorsRadioButton.Checked And num = SCISSORS Then
+            resultLabel.Text = "Computer chose scissors too. Tie!"
+        End If
+
+
+
+
+    End Sub
+End Class
